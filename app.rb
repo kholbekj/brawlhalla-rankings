9b2bb820c545cd3468c686430a0610a69132a06d
Kasper Holbek Jensen <kholbekj@gmail.com>
2016-06-15 22:46:37 +0200

    add search endpoint

--- app.rb
+++ app.rb
@@ -20,3 +20,16 @@ end
 get "/" do
   teams.length.to_s
 end
+
+get "/search/:query" do
+  if params[:query] =~ /\d+?-\d+?/
+    low, high = query.split('-').map(&:to_i)
+    search(low, high).to_json
+  elsif params[:query].to_i.to_s == params[:query]
+    elo = params[:query].to_i
+    search(elo).to_json
+  else
+    name = params[:query]
+    search(name).to_json
+  end
+end
