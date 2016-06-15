e7ad16f0e6afb32a99e617e1945a103c46865621
Kasper Holbek Jensen <kholbekj@gmail.com>
2016-06-15 19:56:49 +0200

    return string

--- app.rb
+++ app.rb
@@ -1,12 +1,13 @@
 require 'sinatra'
 require 'memcachier'
 require 'dalli'
+require 'JSON'
 
 set :cache, Dalli::Client.new
 
 def teams
   @teams ||= settings.cache.fetch("teams")
-  @teams || []
+  (@teams || []).to_json
 end
 
 get "/" do
