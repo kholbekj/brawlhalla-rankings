361f3dc12fc6d9448e3dec12ce21f9969e82b7da
Kasper Holbek Jensen <kholbekj@gmail.com>
2016-06-16 09:43:54 +0200

    fix nomethod

--- shared.rb
+++ shared.rb
@@ -30,7 +30,7 @@ class Cache
   # Fetch and concatenate sharded object
   def self.get_teams
     (0..99).flat_map do |index|
-      @cache_client.get("teams-#{index}").reject(&:nil)
+      @cache_client.get("teams-#{index}").reject(&:nil?)
     end
   end
 end
